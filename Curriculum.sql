0eb0c8e67e783800d9b0e78aa99a043aa837b377
Analiaperullini <analiaperullini@gmail.com>
2023-06-20 14:58:48 -0300

    Agregar archivos de PortfolioBackEnd

---
+++ Curriculum.sql
@@ -0,0 +1,3 @@
+INSERT INTO persona
+VALUES (1, 'Analia', 'Perullini', 'hola 123',
+'1989-07-28', '154266', 'hola@hola.com', 'Soy analista en sistemas y Diseñadora UX/UI', 'Analia Perullini', 'www.foto.com/foto')
